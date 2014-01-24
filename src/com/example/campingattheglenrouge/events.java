54e40535ce10e3a10c72a6c5a5554307e0aa0d55
CollinStubbs <collin.stubbs@uoit.net>
2014-01-24 12:14:05 -0500

    added some markers for maps api

---
+++ src/com/example/campingattheglenrouge/events.java
@@ -0,0 +1,19 @@
+package com.example.campingattheglenrouge;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+ 
+public class events extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_activity);
+        
+    }
+
+
+}
